9cb1a855a2bef6573e2978032a8e770b22816a8a
dipaliii <35723293+dipaliii@users.noreply.github.com>
2019-08-27 12:30:32 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,13 @@
+a="dipali mishra"
+print(a)
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=a+b
+print(c)
+print("Addition of " +str(a)+ "and"+ str(b)+ "is"+ str(c))
+print(f'addition of {a} and {b} is {c}')
+print("addition of",a, "and",b, "is" ,c)
+
+
+
+
