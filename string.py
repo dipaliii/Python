3a835ae058fe2aea2cbc01fd5a78d711cdd84254
dipaliii <35723293+dipaliii@users.noreply.github.com>
2019-09-03 11:02:22 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+#string operations
+S="Dipali Mishra and Gayatri Mishra"
+Z=S[0:1]
+print(Z)
+C=S.replace("Dipali", "AK")
+print(C)
+print("My name is ",'"Dipali Mishra"' ,"and my mother name is" ,'"Gayatri Mishra"')
+A=S[18:]
+print(A)
+D=S.split()
+print(D)
+F=" and Ak Mishra"
+print(S+F)
+G=S.upper()
+print(G)
+print("******",S,"*******")
